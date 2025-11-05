fa8df5dc813102ffe90bde4dc4a89b901722cdba
rnignon <86004439+rnignon@users.noreply.github.com>
2025-11-05 16:24:29 +0900

    [#3] 공통 모듈 코드 작성 (#4)
    * del: tmp 파일 삭제
    
    * feat: BaseEntity, BaseCreateEntity 작성
    
    * chore: common 모듈의 의존성 추가
    
    * feat: ErrorCode, ApiErrorCode 작성
    
    * feat: 공통 예외 클래스 및 예외 핸들러 코드 추가
    
    * feat: 공통 예외 응답 및 페이징 응답 정의
    
    * del: 불필요한 의존성 및 코드 제거

---
+++ common/src/main/java/exception/GlobalException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class GlobalException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public GlobalException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
