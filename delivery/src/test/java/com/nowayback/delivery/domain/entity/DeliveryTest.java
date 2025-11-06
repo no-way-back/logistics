8d80a314e316a531141e9cf7b6c1f69f49b7e2c6
rnignon <rnignon@naver.com>
2025-11-07 00:55:22 +0900

    test: 배송 수령인 정보 수정 시 null 검증 로직 테스트 추가

--- delivery/src/test/java/com/nowayback/delivery/domain/entity/DeliveryTest.java
+++ delivery/src/test/java/com/nowayback/delivery/domain/entity/DeliveryTest.java
@@ -215,4 +215,28 @@ class DeliveryTest {
         /* then */
         assertThat(delivery.getRecipientInfo()).isEqualTo(newRecipientInfo);
     }
+
+    @Test
+    @DisplayName("배송 수령인 정보 수정 시 수령인 정보는 null일 수 없다.")
+    void updateRecipientInfo_ShouldNotAllowNullRecipientInfo() {
+        /* given */
+        OrderId orderId = OrderId.of(UUID.randomUUID());
+        HubId sourceHubId = HubId.of(UUID.randomUUID());
+        HubId destinationHubId = HubId.of(UUID.randomUUID());
+
+        String deliveryAddress = "서울특별시 중구 다산로46길 17 119호";
+        String recipientName = "홍길동";
+        String recipientSlackId = "slack_1234";
+        RecipientInfo recipientInfo = RecipientInfo.of(deliveryAddress, recipientName, recipientSlackId);
+
+        DeliveryManagerId companyDeliveryManagerId = DeliveryManagerId.of(UUID.randomUUID());
+
+        /* when */
+        Delivery delivery = Delivery.create(orderId, sourceHubId, destinationHubId, recipientInfo, companyDeliveryManagerId);
+
+        /* then */
+        assertThatThrownBy(() -> {
+            delivery.updateRecipientInfo(null);
+        }).isInstanceOf(InvalidObjectException.class);
+    }
 }
