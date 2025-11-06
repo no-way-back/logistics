e7a4184285398b1c1b01f367a53f506948923a12
rnignon <rnignon@naver.com>
2025-11-07 02:01:37 +0900

    del: 불필요한 import 문 제거

--- delivery/src/test/java/com/nowayback/delivery/domain/entity/DeliveryTest.java
+++ delivery/src/test/java/com/nowayback/delivery/domain/entity/DeliveryTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.lang.reflect.Field;
 import java.util.UUID;
