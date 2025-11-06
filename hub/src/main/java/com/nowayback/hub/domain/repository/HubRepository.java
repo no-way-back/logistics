d3a100233e6a5e5b1cad72be83690c9854eb360a
Cro <junsj1230@naver.com>
2025-11-07 00:45:05 +0900

    del: 안 쓰는 import 삭제

--- hub/src/main/java/com/nowayback/hub/domain/repository/HubRepository.java
+++ hub/src/main/java/com/nowayback/hub/domain/repository/HubRepository.java
@@ -2,8 +2,6 @@ package com.nowayback.hub.domain.repository;
 
 import com.nowayback.hub.domain.entity.HubEntity;
 
-import java.util.Optional;
-
 public interface HubRepository {
     boolean existsByName(String name);
     HubEntity save(HubEntity any);
