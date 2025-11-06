fb0a6e8d49f309c75af0cd5dba29fbc8aca20d0b
rnignon <rnignon@naver.com>
2025-11-07 01:59:51 +0900

    feat: DeliveryStatus의 상태 전이 검증 메서드 추가

--- delivery/src/main/java/com/nowayback/delivery/domain/delivery/vo/DeliveryStatus.java
+++ delivery/src/main/java/com/nowayback/delivery/domain/delivery/vo/DeliveryStatus.java
@@ -5,17 +5,44 @@ import lombok.Getter;
 @Getter
 public enum DeliveryStatus {
 
-    WAITING_AT_HUB("허브 대기 중"),
-    TRANSIT_BETWEEN_HUBS("허브 이동 중"),
-    AT_DESTINATION_HUB("목적지 허브 도착"),
-    OUT_FOR_DELIVERY("업체 이동 중"),
-    DELIVERED("배송 완료");
+    WAITING_AT_HUB("허브 대기 중") {
+        @Override
+        public boolean canTransitionTo(DeliveryStatus newStatus) {
+            return newStatus == TRANSIT_BETWEEN_HUBS;
+        }
+    },
+    TRANSIT_BETWEEN_HUBS("허브 이동 중") {
+        @Override
+        public boolean canTransitionTo(DeliveryStatus newStatus) {
+            return newStatus == AT_DESTINATION_HUB;
+        }
+    },
+    AT_DESTINATION_HUB("목적지 허브 도착") {
+        @Override
+        public boolean canTransitionTo(DeliveryStatus newStatus) {
+            return newStatus == OUT_FOR_DELIVERY;
+        }
+    },
+    OUT_FOR_DELIVERY("업체 이동 중") {
+        @Override
+        public boolean canTransitionTo(DeliveryStatus newStatus) {
+            return newStatus == DELIVERED;
+        }
+    },
+    DELIVERED("배송 완료") {
+        @Override
+        public boolean canTransitionTo(DeliveryStatus newStatus) {
+            return false;
+        }
+    };
 
     private final String description;
 
     DeliveryStatus(String description) {
         this.description = description;
     }
+
+    public abstract boolean canTransitionTo(DeliveryStatus newStatus);
 
     public boolean canUpdateRecipientInfo() {
         return this != OUT_FOR_DELIVERY && this != DELIVERED;
