e3ba73620049c8ee6b3ead1e15b62e0d7998add1
Cro <junsj1230@naver.com>
2025-11-07 02:54:26 +0900

    feat: 허브 생성 메서드 구현

---
+++ hub/src/main/java/com/nowayback/hub/application/HubService.java
@@ -0,0 +1,36 @@
+package com.nowayback.hub.application;
+
+import com.nowayback.hub.application.command.CreateHubCommand;
+import com.nowayback.hub.application.dto.HubResult;
+import com.nowayback.hub.domain.entity.HubEntity;
+import com.nowayback.hub.domain.repository.HubRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HubService {
+
+    private final HubRepository hubRepository;
+
+    public HubResult create(CreateHubCommand command) {
+        validateDuplicateName(command.name());
+        validateDuplicateAddress(command.address());
+
+        HubEntity hubEntity = HubEntity.create(command);
+
+        return HubResult.from(hubRepository.save(hubEntity));
+    }
+
+    private void validateDuplicateName(String name) {
+        if (hubRepository.existsByName(name)) {
+            throw new IllegalArgumentException("이미 존재하는 허브 이름입니다.");
+        }
+    }
+
+    private void validateDuplicateAddress(String address) {
+        if (hubRepository.existsByAddress(address)) {
+            throw new IllegalArgumentException("이미 존재하는 허브 주소입니다.");
+        }
+    }
+}
