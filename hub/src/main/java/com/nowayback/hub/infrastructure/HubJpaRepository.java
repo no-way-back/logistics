ea243b30dca6f7d81fcfb67c718c9e7cfa84f598
Cro <junsj1230@naver.com>
2025-11-07 00:44:14 +0900

    test: 허브 생성 테스트 코드 작성
    도메인 규칙에 대한 테스트는 Entity 내부에서 진행하였기 때문에 따로 하지 않았습니다

---
+++ hub/src/main/java/com/nowayback/hub/infrastructure/HubJpaRepository.java
@@ -0,0 +1,10 @@
+package com.nowayback.hub.infrastructure;
+
+import com.nowayback.hub.domain.entity.HubEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface HubJpaRepository extends JpaRepository<HubEntity, UUID> {
+    boolean existsByName(String name);
+}
