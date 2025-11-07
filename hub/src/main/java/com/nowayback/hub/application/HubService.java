7fd5d8071f51a520737d64b20430773763173921
Cro <junsj1230@naver.com>
2025-11-08 00:27:56 +0900

    refactor: 커스텀 에러 생성하여 서비스 코드 수정

--- hub/src/main/java/com/nowayback/hub/application/HubService.java
+++ hub/src/main/java/com/nowayback/hub/application/HubService.java
@@ -2,10 +2,14 @@ package com.nowayback.hub.application;
 
 import com.nowayback.hub.application.command.CreateHubCommand;
 import com.nowayback.hub.application.dto.HubResult;
+import com.nowayback.hub.application.exception.HubApplicationErrorCode;
+import com.nowayback.hub.application.exception.HubApplicationException;
 import com.nowayback.hub.domain.entity.Hub;
 import com.nowayback.hub.domain.repository.HubRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.nowayback.hub.application.exception.HubApplicationErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -24,13 +28,13 @@ public class HubService {
 
     private void validateDuplicateName(String name) {
         if (hubRepository.existsByName(name)) {
-            throw new IllegalArgumentException("이미 존재하는 허브 이름입니다.");
+            throw new HubApplicationException(HUB_NAME_ALREADY_EXISTS_EXCEPTION);
         }
     }
 
     private void validateDuplicateAddress(String address) {
         if (hubRepository.existsByAddress(address)) {
-            throw new IllegalArgumentException("이미 존재하는 허브 주소입니다.");
+            throw new HubApplicationException(HUB_ADDRESS_ALEADY_EXISTS_EXCEPTION);
         }
     }
 }
--- hub/src/main/java/com/nowayback/hub/application/exception/HubApplicationErrorCode.java
+++ hub/src/main/java/com/nowayback/hub/application/exception/HubApplicationErrorCode.java
@@ -1,11 +1,12 @@
-package com.nowayback.hub.domain.exception;
+package com.nowayback.hub.application.exception;
 
 import com.nowayback.common.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
-public enum HubDomainErrorCode implements ErrorCode {
+public enum HubApplicationErrorCode implements ErrorCode {
 
-    HUB_NAME_ALREADY_EXISTS_EXCEPTION("HUB1001","이미 존재하는 허브 이름입니다.", HttpStatus.CONFLICT);
+    HUB_NAME_ALREADY_EXISTS_EXCEPTION("HUB2001","이미 존재하는 허브 이름입니다.", HttpStatus.CONFLICT),
+    HUB_ADDRESS_ALEADY_EXISTS_EXCEPTION("HUB2002", "이미 존재하는 허브 주소입니다.", HttpStatus.CONFLICT),;
 
     private final String code;
     private final String message;
@@ -26,7 +27,7 @@ public enum HubDomainErrorCode implements ErrorCode {
         return httpStatus;
     }
 
-    HubDomainErrorCode(String code, String message, HttpStatus httpStatus) {
+    HubApplicationErrorCode(String code, String message, HttpStatus httpStatus) {
         this.code = code;
         this.message = message;
         this.httpStatus = httpStatus;
