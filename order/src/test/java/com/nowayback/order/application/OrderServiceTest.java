19381ad4db3063fc8144d267a3c44afd9a734f72
sejunO <friendlyoh@gmail.com>
2025-11-09 20:43:46 +0900

    test: 주문 생성 시 재고 차감 및 배송 생성 실패 테스트 작성

--- order/src/test/java/com/nowayback/order/application/OrderServiceTest.java
+++ order/src/test/java/com/nowayback/order/application/OrderServiceTest.java
@@ -2,6 +2,7 @@ package com.nowayback.order.application;
 
 import static com.nowayback.order.fixture.OrderFixture.createOrderCommand;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -11,10 +12,12 @@ import com.nowayback.order.application.client.ProductClient;
 import com.nowayback.order.application.client.response.CreateDeliveryResponse;
 import com.nowayback.order.application.client.response.DecreaseStockResponse;
 import com.nowayback.order.application.command.CreateOrderCommand;
+import com.nowayback.order.application.exception.OrderApplicationException;
 import com.nowayback.order.domain.entity.Order;
 import com.nowayback.order.domain.repository.OrderRepository;
 import com.nowayback.order.domain.vo.OrderStatus;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -36,33 +39,70 @@ class OrderServiceTest {
     @InjectMocks
     OrderService orderService;
 
-    @Test
-    @DisplayName("재고 차감과 배송 생성이 완료되면 주문을 생성한다.")
-    void createOrder_success() {
-        // given
-        CreateOrderCommand orderCommand = createOrderCommand();
+    @Nested
+    class createOrder {
+        @Test
+        @DisplayName("재고 차감과 배송 생성이 완료되면 주문을 생성한다.")
+        void createOrder_success() {
+            // given
+            CreateOrderCommand orderCommand = createOrderCommand();
 
-        DecreaseStockResponse decreaseStockResponse = new DecreaseStockResponse(true);
-        CreateDeliveryResponse createDeliveryResponse = new CreateDeliveryResponse(true);
+            DecreaseStockResponse decreaseStockResponse = new DecreaseStockResponse(true);
+            CreateDeliveryResponse createDeliveryResponse = new CreateDeliveryResponse(true);
 
-        given(productClient.decreaseStocks(any())).willReturn(decreaseStockResponse);
-        given(deliveryClient.createDelivery(any())).willReturn(createDeliveryResponse);
+            given(productClient.decreaseStocks(any())).willReturn(decreaseStockResponse);
+            given(deliveryClient.createDelivery(any())).willReturn(createDeliveryResponse);
 
-        // when
-        orderService.createOrder(orderCommand);
+            // when
+            orderService.createOrder(orderCommand);
 
-        //then
-        ArgumentCaptor<Order> orderCaptor = ArgumentCaptor.forClass(Order.class);
-        then(orderRepository).should().save(orderCaptor.capture());
+            //then
+            ArgumentCaptor<Order> orderCaptor = ArgumentCaptor.forClass(Order.class);
+            then(orderRepository).should().save(orderCaptor.capture());
 
-        Order savedOrder = orderCaptor.getValue();
-        assertThat(savedOrder.getSupplierCompanyId()).isEqualTo(orderCommand.supplierCompanyId());
-        assertThat(savedOrder.getSupplierCompanySnapshot()).isEqualTo(orderCommand.supplierCompanySnapshot());
-        assertThat(savedOrder.getReceiverCompanyId()).isEqualTo(orderCommand.receiverCompanyId());
-        assertThat(savedOrder.getReceiverCompanySnapshot()).isEqualTo(
-            orderCommand.receiverCompanySnapshot());
-        assertThat(savedOrder.getStatus()).isEqualTo(OrderStatus.CREATED);
-        assertThat(savedOrder.getRequest()).isEqualTo(orderCommand.request());
-        assertThat(savedOrder.getOrderItems()).hasSize(orderCommand.createOrderItems().size());
+            Order savedOrder = orderCaptor.getValue();
+            assertThat(savedOrder.getSupplierCompanyId()).isEqualTo(orderCommand.supplierCompanyId());
+            assertThat(savedOrder.getSupplierCompanySnapshot()).isEqualTo(orderCommand.supplierCompanySnapshot());
+            assertThat(savedOrder.getReceiverCompanyId()).isEqualTo(orderCommand.receiverCompanyId());
+            assertThat(savedOrder.getReceiverCompanySnapshot()).isEqualTo(
+                orderCommand.receiverCompanySnapshot());
+            assertThat(savedOrder.getStatus()).isEqualTo(OrderStatus.CREATED);
+            assertThat(savedOrder.getRequest()).isEqualTo(orderCommand.request());
+            assertThat(savedOrder.getOrderItems()).hasSize(orderCommand.createOrderItems().size());
+        }
+
+        @Test
+        @DisplayName("재고 차감에 실패하면 예외가 발생한다.")
+        void createOrder_shouldThrowWhenFailDecreaseStock() {
+            // given
+            CreateOrderCommand orderCommand = createOrderCommand();
+
+            DecreaseStockResponse decreaseStockResponse = new DecreaseStockResponse(false);
+
+            given(productClient.decreaseStocks(any())).willReturn(decreaseStockResponse);
+
+            // when / then
+            assertThatThrownBy(() -> {
+                orderService.createOrder(orderCommand);
+            }).isInstanceOf(OrderApplicationException.class);
+        }
+
+        @Test
+        @DisplayName("배송 생성에 실패하면 예외가 발생한다.")
+        void createOrder_shouldThrowWhenFailCreateDelivery() {
+            // given
+            CreateOrderCommand orderCommand = createOrderCommand();
+
+            DecreaseStockResponse decreaseStockResponse = new DecreaseStockResponse(true);
+            CreateDeliveryResponse createDeliveryResponse = new CreateDeliveryResponse(false);
+
+            given(productClient.decreaseStocks(any())).willReturn(decreaseStockResponse);
+            given(deliveryClient.createDelivery(any())).willReturn(createDeliveryResponse);
+
+            // when / then
+            assertThatThrownBy(() -> {
+                orderService.createOrder(orderCommand);
+            }).isInstanceOf(OrderApplicationException.class);
+        }
     }
 }
