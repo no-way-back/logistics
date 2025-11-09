b7a02d089de60090c287afe6c9cde0c8a30448ef
Cro <junsj1230@naver.com>
2025-11-09 23:32:44 +0900

    feat: 허브 컨트롤러 작성

--- hub/src/main/java/com/nowayback/hub/presentation/HubController.java
+++ hub/src/main/java/com/nowayback/hub/presentation/HubController.java
@@ -2,16 +2,18 @@ package com.nowayback.hub.presentation;
 
 import com.nowayback.hub.application.HubService;
 import com.nowayback.hub.application.command.CreateHubCommand;
+import com.nowayback.hub.application.command.UpdateHubCommand;
 import com.nowayback.hub.presentation.request.CreateHubRequest;
+import com.nowayback.hub.presentation.request.UpdateHubRequest;
 import com.nowayback.hub.presentation.response.CreateHubResponse;
+import com.nowayback.hub.presentation.response.UpdateHubResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/hubs")
@@ -22,11 +24,21 @@ public class HubController {
 
     @PostMapping
     public ResponseEntity<CreateHubResponse> create(@Valid @RequestBody CreateHubRequest request) {
-        CreateHubCommand createHubCommand = CreateHubCommand.of(request);
+        CreateHubCommand createHubCommand = CreateHubCommand.from(request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(CreateHubResponse.of(
+                .body(CreateHubResponse.from(
                         hubService.create(createHubCommand))
                 );
+    }
+
+    @PutMapping("/{hubId}")
+    public ResponseEntity<UpdateHubResponse> update(@PathVariable UUID hubId, @RequestBody UpdateHubRequest request) {
+        UpdateHubCommand updateHubCommand = UpdateHubCommand.from(request);
+
+        return ResponseEntity.ok(
+                UpdateHubResponse.from(
+                        hubService.update(hubId, updateHubCommand))
+        );
     }
 }
