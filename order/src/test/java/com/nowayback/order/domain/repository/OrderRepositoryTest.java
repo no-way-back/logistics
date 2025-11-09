815304366963985c834679cc7ee7f28778948e13
sejunO <friendlyoh@gmail.com>
2025-11-09 22:03:51 +0900

    feat: OrderRepository 구현

--- order/src/test/java/com/nowayback/order/domain/repository/OrderRepositoryTest.java
+++ order/src/test/java/com/nowayback/order/domain/repository/OrderRepositoryTest.java
@@ -4,7 +4,7 @@ import static com.nowayback.order.fixture.OrderFixture.createOrder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.nowayback.order.domain.entity.Order;
-import com.nowayback.order.presentation.persistence.OrderRepositoryImpl;
+import com.nowayback.order.infrastructure.persistence.OrderRepositoryImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
