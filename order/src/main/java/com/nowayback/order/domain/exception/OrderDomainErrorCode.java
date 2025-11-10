06095653605a01494528c83c656159085f52a8fa
sejunO <friendlyoh@gmail.com>
2025-11-10 12:59:11 +0900

    refactor: 불필요한 import 삭제 및 오타 수정

--- order/src/main/java/com/nowayback/order/domain/exception/OrderDomainErrorCode.java
+++ order/src/main/java/com/nowayback/order/domain/exception/OrderDomainErrorCode.java
@@ -12,7 +12,7 @@ public enum OrderDomainErrorCode implements ErrorCode {
     INVALID_ORDER_STATUS_TRANSITION("ORDER1005", "유효하지 않은 주문 상태 전환입니다.", HttpStatus.BAD_REQUEST),
     INVALID_ORDER_STATUS_FOR_DELETE("ORDER1006", "배송이 진행 중인 경우에는 주문을 삭제할 수 없습니다.",
         HttpStatus.BAD_REQUEST),
-    INVALID_ORDER_ITEM_QUANTITY("ORDER1007", "주문 수량은 1개 이상이어 합니다.", HttpStatus.BAD_REQUEST),
+    INVALID_ORDER_ITEM_QUANTITY("ORDER1007", "주문 수량은 1개 이상이어야 합니다.", HttpStatus.BAD_REQUEST),
     NULL_PRODUCT_ID("ORDER1008", "주문 상품 ID는 null일 수 없습니다.", HttpStatus.BAD_REQUEST),
     MISSING_ORDER_ITEM_NAME("ORDER1009", "주문 상품 이름은 null이거나 비어 있을 수 없습니다.", HttpStatus.BAD_REQUEST),
     INVALID_ORDER_ITEM_PRICE("ORDER1010", "상품 가격은 0보다 커야 합니다.", HttpStatus.BAD_REQUEST),
--- order/src/main/java/com/nowayback/order/infrastructure/client/feign/DeliveryFeignClient.java
+++ order/src/main/java/com/nowayback/order/infrastructure/client/feign/DeliveryFeignClient.java
@@ -1,11 +1,8 @@
 package com.nowayback.order.infrastructure.client.feign;
 
 import com.nowayback.order.infrastructure.client.feign.request.CreateDeliveryFeignRequest;
-import com.nowayback.order.infrastructure.client.feign.request.DecreaseStockFeignRequest;
 import com.nowayback.order.infrastructure.client.feign.response.CreateDeliveryFeignResponse;
-import com.nowayback.order.infrastructure.client.feign.response.DecreaseStockFeignResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
