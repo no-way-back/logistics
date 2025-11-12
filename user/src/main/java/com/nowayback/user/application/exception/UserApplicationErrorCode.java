cc153e9a72c116a9d8add022d49a3bc6800de617
Mybread2 <ckwnsgh4@naver.com>
2025-11-12 11:12:07 +0900

    refactor: errorcode 수정

--- user/src/main/java/com/nowayback/user/application/exception/UserApplicationErrorCode.java
+++ user/src/main/java/com/nowayback/user/application/exception/UserApplicationErrorCode.java
@@ -7,19 +7,19 @@ import com.nowayback.common.exception.ErrorCode;
 public enum UserApplicationErrorCode implements ErrorCode {
 
 	// 조회 실패
-	USER_NOT_FOUND("USER_2001", "사용자를 찾을 수 없습니다", HttpStatus.NOT_FOUND),
+	USER_NOT_FOUND("USER2001", "사용자를 찾을 수 없습니다", HttpStatus.NOT_FOUND),
 
 	// 중복
-	USER_ALREADY_EXISTS("USER_2002", "이미 존재하는 사용자입니다", HttpStatus.CONFLICT),
+	USER_ALREADY_EXISTS("USER2002", "이미 존재하는 사용자입니다", HttpStatus.CONFLICT),
 
 	// 인증 실패
-	INVALID_PASSWORD("USER_2003", "비밀번호가 일치하지 않습니다", HttpStatus.UNAUTHORIZED),
-	USER_NOT_APPROVED("USER_2004", "승인되지 않은 사용자입니다", HttpStatus.FORBIDDEN),
+	INVALID_PASSWORD("USER2003", "비밀번호가 일치하지 않습니다", HttpStatus.UNAUTHORIZED),
+	USER_NOT_APPROVED("USER2004", "승인되지 않은 사용자입니다", HttpStatus.FORBIDDEN),
 
 	// 기타
-	USER_ALREADY_DELETED("USER_2005", "이미 삭제된 사용자입니다", HttpStatus.BAD_REQUEST),
-	INVALID_APPROVAL_STATUS("USER_2006", "유효하지 않은 승인 상태입니다", HttpStatus.BAD_REQUEST),
-	INSUFFICIENT_PERMISSION("USER_2007", "권한이 부족합니다", HttpStatus.FORBIDDEN);
+	USER_ALREADY_DELETED("USER2005", "이미 삭제된 사용자입니다", HttpStatus.BAD_REQUEST),
+	INVALID_APPROVAL_STATUS("USER2006", "유효하지 않은 승인 상태입니다", HttpStatus.BAD_REQUEST),
+	INSUFFICIENT_PERMISSION("USER2007", "권한이 부족합니다", HttpStatus.FORBIDDEN);
 
 	private final String code;
 	private final String message;
--- user/src/main/java/com/nowayback/user/domain/exception/UserDomainErrorCode.java
+++ user/src/main/java/com/nowayback/user/domain/exception/UserDomainErrorCode.java
@@ -7,16 +7,16 @@ import com.nowayback.common.exception.ErrorCode;
 public enum UserDomainErrorCode implements ErrorCode {
 
 	// 상태 관련
-	ALREADY_PROCESSED("USER_1001","이미 처리된 회원가입 요청입니다", HttpStatus.BAD_REQUEST),
-	INVALID_USER_STATUS("USER_1002", "유효하지 않은 사용자 상태입니다", HttpStatus.BAD_REQUEST),
+	ALREADY_PROCESSED("USER1001","이미 처리된 회원가입 요청입니다", HttpStatus.BAD_REQUEST),
+	INVALID_USER_STATUS("USER1002", "유효하지 않은 사용자 상태입니다", HttpStatus.BAD_REQUEST),
 
 	// 검증 관련
-	INVALID_USERNAME_FORMAT("USER_1003","사용자 이름은 4-10자의 영문 소문자와 숫자만 가능합니다", HttpStatus.BAD_REQUEST),
-	USERNAME_REQUIRED("USER_1004","사용자 이름은 필수입니다", HttpStatus.BAD_REQUEST),
-	INVALID_APPROVAL_STATUS("USER_1005","올바른 상태값이 아닙니다.", HttpStatus.BAD_REQUEST),
+	INVALID_USERNAME_FORMAT("USER1003","사용자 이름은 4-10자의 영문 소문자와 숫자만 가능합니다", HttpStatus.BAD_REQUEST),
+	USERNAME_REQUIRED("USER1004","사용자 이름은 필수입니다", HttpStatus.BAD_REQUEST),
+	INVALID_APPROVAL_STATUS("USER1005","올바른 상태값이 아닙니다.", HttpStatus.BAD_REQUEST),
 
 	// 삭제 관련
-	USER_ALREADY_DELETED("USER_1006", "이미 삭제된 사용자입니다.", HttpStatus.BAD_REQUEST);
+	USER_ALREADY_DELETED("USER1006", "이미 삭제된 사용자입니다.", HttpStatus.BAD_REQUEST);
 
 	private final String code;
 	private final String message;
